8e6bb10a4a40f6af1708cb64abc0a767146f29c2
Leif Meyer <leif@leifmeyer.io>
2020-12-11 12:56:58 -0800

    Updated the rele.py script to test stream providing.

--- rele.py
+++ rele.py
@@ -5,17 +5,25 @@ from pathlib import Path
 
 redis_client = redis.Redis(**get_option('redis'))
 
-from rele import BatchProvider
+from rele import BatchProvider, ProviderStreamInfo
 
 data_path = Path(__file__).parent / 'output' / 'fabrica' / 'xy.csv'
 data = data_path.read_text()
 chunks = [c + '\n' for c in data.split('\n')[1:-1]]
+
+stream_info = ProviderStreamInfo(
+  key='RELE_BROADCAST_STREAM',
+  metadata={
+    'source': 'rele_script',
+  }
+)
 
 provider = BatchProvider(
   redis=redis_client,
   broadcast_channel='RELE_BROADCAST_CHANNEL',
   batch_size=10,
   rate=1,
-  delay=3
+  delay=3,
+  stream_info=stream_info
 )
 provider.enqueue(chunks=chunks)
