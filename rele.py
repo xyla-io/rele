fd236f0d1b6270f16e72bbfe2cbf0380f0bbedd6
Leif Meyer <leif@leifmeyer.io>
2020-12-11 12:32:18 -0800

    Created a BatchProvider class and updated the rele.py script to test it.

--- rele.py
+++ rele.py
@@ -1,13 +1,21 @@
 import redis
 
 from environment import get_option
+from pathlib import Path
 
 redis_client = redis.Redis(**get_option('redis'))
 
-from rele import Provider
+from rele import BatchProvider
 
-provider = Provider(
+data_path = Path(__file__).parent / 'output' / 'fabrica' / 'xy.csv'
+data = data_path.read_text()
+chunks = [c + '\n' for c in data.split('\n')[1:-1]]
+
+provider = BatchProvider(
   redis=redis_client,
-  broadcast_channel='RELE_BROADCAST_CHANNEL'
+  broadcast_channel='RELE_BROADCAST_CHANNEL',
+  batch_size=10,
+  rate=1,
+  delay=3
 )
-provider.feed(data='1,2\n3,4\n5,6\n7,8')
+provider.enqueue(chunks=chunks)
--- rele/provider.py
+++ rele/provider.py
@@ -1,4 +1,6 @@
 from redis import Redis
+from typing import List
+from time import sleep
 
 class Provider:
   r: Redis
@@ -10,3 +12,44 @@ class Provider:
   
   def feed(self, data: str):
     self.r.publish(self.broadcast_channel, data)
+
+class BatchProvider(Provider):
+  queue: List[str]
+  batch_size: int
+  rate: float
+  delay: float
+
+  def __init__(self, redis: Redis, broadcast_channel: str, batch_size: int, rate: float, delay: float=0):
+    self.queue = []
+    self.batch_size = batch_size
+    self.rate = rate
+    self.delay = delay
+    super().__init__(
+      redis=redis,
+      broadcast_channel=broadcast_channel
+    )
+
+  def enqueue(self, chunks: List[str]):
+    self.queue.extend(chunks)
+    if self.delay > 0:
+      sleep(self.delay)
+      
+    chunk_index = 0
+    batch = ''
+    def feed_batch():
+      nonlocal chunk_index, batch
+      if not chunk_index:
+        return
+      self.feed(batch)
+      chunk_index = 0
+      batch = ''
+      if self.rate > 0:
+        sleep(self.rate)
+
+    for chunk in chunks:
+      chunk_index = chunk_index + 1
+      if chunk_index < self.batch_size:
+        batch = batch + chunk
+        continue
+      feed_batch()
+    feed_batch()
